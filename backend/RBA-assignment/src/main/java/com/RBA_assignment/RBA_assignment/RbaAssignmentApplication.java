ee8e826f35cfe50834af479ab753facac114ddc9
Matija Topolovec <c-mtopolovec@globalization-partners.com>
2025-08-03 16:56:42 +0200

    Add missing files

---
+++ backend/RBA-assignment/src/main/java/com/RBA_assignment/RBA_assignment/RbaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.RBA_assignment.RBA_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbaAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbaAssignmentApplication.class, args);
+	}
+
+}
---
+++ backend/RBA-assignment/src/test/java/com/RBA_assignment/RBA_assignment/RbaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RBA_assignment.RBA_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
